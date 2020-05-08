df7a8e38ff09c82a691cbe18ff840e0a97b4ebc0
Markham <quinesquines@gmail.com>
2020-05-08 14:43:08 -0700

    Allow user to hit "Enter" to break snooze or to start stretch early on the daemon terminal

--- stretch-timer-daemon.sh
+++ stretch-timer-daemon.sh
@@ -14,18 +14,32 @@ iteration () {
 now () {
   date +%s
 }
+is_screensaver_active () {
+  gdbus call -e -o /org/gnome/ScreenSaver -d org.gnome.ScreenSaver -m org.gnome.ScreenSaver.GetActive | grep -s true
+}
 mainloop () {
 	while ((1)); do
     tput sc
     DUE=$(( $(now) + (20 * 60) ))
     while [[ $(now) -lt $DUE ]]; do
-      if [[ -t 1 ]]; then
-        tput rc; tput ed;
+      if [[ -t 1 ]]; then # If fd 1 is a terminal
+        tput cr; tput ed;
         REMAINING=$(( $DUE - $(now) ))
         printf "Remaining: %s" "$(date -d@$REMAINING -u +%H:%M:%S)"
       fi
-      sleep 1
+      # Allow user to hit 'Enter' to break the loop and start stretches
+      read -s -N1 -t1 KBD_INPUT
+      if [[ $KBD_INPUT == $'\x0a' ]]; then
+        break
+      fi
     done
+    # Spinlock if screensaver is active
+    tput cr; tput ed;
+    is_screensaver_active && echo "The screensaver was active"
+    while is_screensaver_active; do
+      sleep 60
+    done
+    # Start stretches interface
 		iteration
 	done
 }
--- stretch-timer-task.sh
+++ stretch-timer-task.sh
@@ -5,36 +5,34 @@ WAIT_TIMEOUT=240 # auto-snooze after `read` timeout
 
 KEEP_LOOPING=1
 
+is_screensaver_active () {
+  gdbus call -e -o /org/gnome/ScreenSaver -d org.gnome.ScreenSaver -m org.gnome.ScreenSaver.GetActive | grep -s true
+}
 snooze () {
-	if (($#)); then
-		MINUTES=$1
-	else
-		read -t $PROMPT_TIMEOUT -p "Enter number of minutes: " MINUTES
-	fi
-	if [[ -z $MINUTES ]]; then
-		sleep $WAIT_TIMEOUT
-		KEEP_LOOPING=1
-	else
-		echo -e "\n$MINUTES minutes snooze..."
-		tput sc
-		while [[ $MINUTES -gt 0 ]]; do
-			clear
-			printf "Snoozing $MINUTES minutes..."
-			sleep 60
-			MINUTES=$(( $MINUTES - 1 ))
-		done
-		echo -e "\nSnooze done"
-	fi
+	local MINUTES=$1
+	local DEADLINE=$(( $(date +%s) + ($MINUTES * 60) ))
+	tput cr; tput sc
+	while [[ $(date +%s) -lt $DEADLINE ]]; do
+		local DELTA=$(( $DEADLINE - $(date +%s) ))
+		clear; printf "Snoozing $(date -u -d @${DELTA} +%H:%M:%S)... (press Enter to end)"
+		read -t 1 -N 1 KBD_IN # Wait 1 second (or until user input)
+		if [[ $KBD_IN == $'\x0a' ]]; then
+			start
+			return
+		fi
+	done
+	echo -e "\nSnooze done"
 }
 clear () { tput rc; tput ed; }
 reverse_video () { printf '\e[?5h'; }
 normal_video () { printf '\e[?5l'; }
 activate () { xdotool search --name 'Stretch Break' windowactivate; }
 flash () {
-	REP=${1:-6}
-	SEC=${2:-0.1}
+	REP=${1:-6} # How many times to flash
+	SEC=${2:-0.1} # How many seconds to wait between flashes
+	DOBEL=${3:-1} # Whether to sound the bell
 	for i in `seq $REP`; do
-		tput bel
+		(($DOBEL)) && tput bel
 		reverse_video
 		sleep $SEC
 		normal_video
@@ -58,15 +56,20 @@ start () {
 
 mainmenu () {
 	activate
-	flash 4
-	echo -e "Stretch break\a"
+	# Spinlock if screensaver active
+  is_screensaver_active && echo "The screensaver was active"
+  while is_screensaver_active; do
+    sleep 60
+  done
+	# Run
+	flash 4 0.1 0
+	clear; echo -e -n "Stretch break.\a "
 	read -n 999999 -s -t 0.01 DISCARD
 	read -n 1 -s -p "Press any key: " -t $PROMPT_TIMEOUT OPT
 	read -n 999999 -s -t 0.01 DISCARD
 	if [[ -n $OPT ]]; then
 		case $OPT in
 			[123456789]) snooze $OPT;;
-			s) snooze;;
 			q) KEEP_LOOPING=0;;
 			*) start;;
 		esac
@@ -76,7 +79,7 @@ mainmenu () {
 }
 
 activate
-flash 1
+flash 1 0.1 0
 while (($KEEP_LOOPING)); do
 	mainmenu
 done
